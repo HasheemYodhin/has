63f13fa64987b071ccef31f81c243812579397c9
Hasheem <hmasthan@gmail.com>
2025-09-05 13:21:47 +0200

    3rd commit done

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+ 
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "hasheem_bucket" {
+  bucket = "hasheem-demo-bucket"
+  tags = {
+    Name        = "Hasheem's Bucket"
+    Environment = "Dev"
+  }
+}
